4964bf1d34332580ef024cdc719f619970eeeeaa
N4N001 <nanut120@gmail.com>
2021-05-04 11:38:21 +0200

    oprava index.php

--- index.php
+++ index.php
@@ -9,24 +9,38 @@
 <body>
     <form action="submit.php" method="post">
     <h1>Registration</h1>
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" placeholder="Email">
+        <br>
+        <label for="password">Password:</label>
+        <input typ="password" name="password" id="password" placeholder="Password">
+        <br>
+        <label for="question">Choose a control question:</label>
+        <select id="question" name="question">
+        <option value="mothername">What was your mother's maiden name?</option>
+        <option value="parentscity">In what city did your parents meet?</option>
+        <option value="petname">What was the name of your first pet?</option>
+        </select>
+        <br>
+        <label for="answer">Answer your control question:</label>
+        <input type="text" name="answer" id="answer" placeholder="Control answer">
+        <br>
         <label for="firstname">Firstname:</label>
         <input type="text" name="firstname" id="firstname" placeholder="Firstname">
         <br>
         <label for="lastname">Lastname:</label>
         <input type="text" name="lastname" id="lastname" placeholder="Lastname">
         <br>
-        <label for="password">Password:</label>
-        <input type="password" name="password" id="password" placeholder="Password">
-        <input type="submit" value="Login">
+        <label for="gender">What is your gender?</label>
         <br>
-        What is your gender?
         <input type="radio" id="male" name="gender" value="male">
         <label for="male">Male</label><br>
         <input type="radio" id="female" name="gender" value="female">
         <label for="female">Female</label><br>
         <input type="radio" id="unknown" name="gender" value="unknown">
         <label for="unknown">Prefer not to say</label>
-
+        <br>
+        <input type="submit" value="Register">
     </form>
 </body>
 </html>
